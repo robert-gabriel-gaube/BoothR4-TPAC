48ad75ec801347659a783bd6be7cd8805d019f5d
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2024-11-13 21:00:23 +0200

    Changed RegM

--- GeneralRegisters/RegM/TestRegM.vhd
+++ GeneralRegisters/RegM/TestRegM.vhd
@@ -1,20 +1,24 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
 ENTITY test IS
 END test;
 
 ARCHITECTURE t OF test IS
     COMPONENT RegM IS 
         PORT(
-            clk, rst_b, c0: IN bit;
-            input: IN bit_vector(7 DOWNTO 0);
-            output: OUT bit_vector(7 DOWNTO 0)
+            clk, rst_b, c0: IN std_logic;
+            input: IN std_logic_vector(7 DOWNTO 0);
+            output: OUT std_logic_vector(7 DOWNTO 0)
         );
     END COMPONENT;
 
     CONSTANT ClockFrequency : integer := 1; -- 1 Hz
     CONSTANT ClockPeriod    : time    := 100 ps / ClockFrequency;
 
-    SIGNAL clk_s, rst_b_s, c0_s: bit;
-    SIGNAL input_s, output_s: bit_vector(7 DOWNTO 0);
+    SIGNAL clk_s, rst_b_s, c0_s: std_logic:= '0';
+    SIGNAL input_s, output_s: std_logic_vector(7 DOWNTO 0);
 
 BEGIN
 
@@ -32,7 +36,7 @@ BEGIN
     clk_s <= NOT clk_s AFTER ClockPeriod / 2;
 
     input_s <=  x"1F", 
-                x"1E" AFTER ClockPeriod, 
+                x"1E" AFTER 1 * ClockPeriod, 
                 x"1D" AFTER 2 * ClockPeriod, 
                 x"1C" AFTER 3 * ClockPeriod,
                 x"1B" AFTER 4 * ClockPeriod,
