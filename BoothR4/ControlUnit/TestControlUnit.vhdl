3d0c60cfe2c4dfd8e3685c0ce20142225d8a77a1
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2024-12-07 15:19:26 +0200

    Reverted changes on ControlUnit testbench

--- BoothR4/ControlUnit/TestControlUnit.vhdl
+++ BoothR4/ControlUnit/TestControlUnit.vhdl
@@ -13,8 +13,7 @@ ARCHITECTURE tb OF TestControlUnit IS
             bgn : IN std_logic;
             q1, q0, q : IN std_logic;
             is_count_3 : IN std_logic;
-            c0, c1, c2, c3, c4, c5, c6, done : OUT std_logic;
-            state_out : OUT std_logic_vector(3 downto 0)
+            c0, c1, c2, c3, c4, c5, c6, done : OUT std_logic
         );
     END COMPONENT;
 
@@ -24,9 +23,8 @@ ARCHITECTURE tb OF TestControlUnit IS
     SIGNAL q1_s, q0_s, q_s : std_logic := '0';
     SIGNAL is_count_3_s : std_logic := '0';
     SIGNAL c0_s, c1_s, c2_s, c3_s, c4_s, c5_s, c6_s, done_s : std_logic;
-    SIGNAL state_s : std_logic_vector(3 downto 0);
 
-    CONSTANT clk_period : time := 100 ps;
+    CONSTANT clk_period : time := 10 ns;
 
 BEGIN
     cu: ControlUnit
@@ -45,8 +43,7 @@ BEGIN
             c4 => c4_s,
             c5 => c5_s,
             c6 => c6_s,
-            done => done_s,
-            state_out => state_s
+            done => done_s
         );
 
     clk_process: PROCESS
@@ -91,7 +88,7 @@ BEGIN
         q1_s <= '1';
         q0_s <= '1';
         q_s <= '0';
-        WAIT FOR 2 * clk_period;
+        WAIT FOR clk_period;
 
         -- Test 6: Tranzi?ie �n S6
         -- Rezultate a?teptate: FSM �n S6, c2 = '1', c3 = '1', restul '0'.
